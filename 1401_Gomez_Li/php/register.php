c5fd3839878dc6f812f3a95382698bbaf764714a
Carlos Li Hu <carloslihu96@gmail.com>
2017-10-09 00:09:24 +0200

    register sin acabar
    los controles de campos de registro se hacen en javascript al parecer (?)

--- 1401_Gomez_Li/php/register.php
+++ 1401_Gomez_Li/php/register.php
@@ -7,15 +7,29 @@
     <?php
     //definir variables
     $username = $email = $password = $password2 = $bank_account="";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+    //debo mirar si estos controles los hago mejor en el cliente
+    /*if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["username"])) {
+            $usernameErr = "Username is required";
+        }else{
+            $username=test_input($_POST["username"]);
+        }
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is required";
+        } else {
+            $email = test_input($_POST["email"]);
+            // check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+              $emailErr = "Invalid email format"; 
+            }
+        }
     }
     function test_input($data) {
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
         return $data;
-    }
+    }*/
     ?>
         <?php include 'includes/header.php';?>
 
