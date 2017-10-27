5ac00f83cd2f53aec7bea5e11222f91c29cd94fe
Carlos Li Hu <carloslihu96@gmail.com>
2017-10-27 17:28:58 +0200

    Auto stash before merge of "master" and "origin/master"

--- 1401_Gomez_Li/php/index.php
+++ 1401_Gomez_Li/php/index.php
@@ -16,28 +16,44 @@
                 <h2>Home</h2>
 
                 <div>
-                    <form action="" method="post">
+                    <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]); ?>" method="post">
 
                         <input type="text" name="search" placeholder="Search..">
                         <select name="filtro">
-                            <option value="0">Seleccionar...</option> 
-                            <option value="1">Pelicula</option> 
-                            <option value="2">Genero</option> 
-                            <option value="3">Director</option>
+                            <option value="titulo">titulo</option> 
+                            <option value="categoria">categoria</option> 
+                            <option value="director">director</option>
                         </select>
-                        <input type="submit" value="search">
+                        <input type="submit" value="submit">
                     </form>
                 </div>
 
 
                 <?php
-                if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                    
-                }
                 $xml = simplexml_load_file("../xml/catalogo.xml") or die("Error: Cannot create object");
-
-                foreach ($xml->children() as $films) {
-                    echo '<div class="responsive">
+                if (isset($_REQUEST['search'])) {
+                    echo 'estoy dentro';
+                    $filtro = $_REQUEST['filtro'];
+                    $search = $_REQUEST['search'];
+                    echo $search . '     ' . $filtro;
+                    $films = $xml->xpath("/catalogo/pelicula['.$filtro.'='.$search.']");
+                    foreach ($films as $film) {
+                        echo '<div class="responsive">
+                    <div class="gallery">
+                        <a href="product.php?id=' . $film->id . '">
+                        <img src=' . $film->poster . ' alt="imagen" width="100" height="100">
+                    </a>
+                        <div class="desc">
+                            <a href="product.php?id=' . $film->id . '">
+                            <b>' . $film->titulo . '</b>
+                        </a>
+                            <br>' . $film->director . '</div>
+                    </div>
+                </div>';
+                    }
+                } else {
+                    foreach ($xml->children() as $films) {
+                        echo '<div class="responsive">
                     <div class="gallery">
                         <a href="product.php?id=' . $films->id . '">
                         <img src=' . $films->poster . ' alt="imagen" width="100" height="100">
@@ -49,6 +65,7 @@
                             <br>' . $films->director . '</div>
                     </div>
                 </div>';
+                    }
                 }
                 ?>
                 <div class="clearfix"></div>
