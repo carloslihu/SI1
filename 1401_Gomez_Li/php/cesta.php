67498972f3c367e972a89936e8ed078ec52202c7
Unknown <javiergomez_1996@hotmail.com>
2017-10-27 17:52:11 +0200

    utils anñadido

--- 1401_Gomez_Li/php/cesta.php
+++ 1401_Gomez_Li/php/cesta.php
@@ -19,6 +19,7 @@
                 <?php
                     //codigo que procesa los post y actua ante ellos:
                     include 'includes/fcesta.php';
+                    include 'includes/utils.php';
 
                     if(isset($_POST['comprar'])){//el usuario ha pedido comprar los productos de su carrito
                         if(isset($_SESSION['username']))
@@ -38,22 +39,13 @@
                             </form>';
                     foreach($_SESSION['cesta'] as $ids) { 
                         $film = $xml->xpath("/catalogo/pelicula[id='$ids']")[0];
-                        echo '<div class="responsive">
-                            <div class="gallery">
-                                <a href="product.php?title='.$film->titulo.'">
-                                <img src='.$film->poster.' alt="imagen" width="100" height="100">
-                            </a>
-                                <div class="desc">
-                                    <a href="product.php?title='.$film->titulo.'">
-                                    <b>'.$film->titulo.'</b>
-                                </a>
-                                    <br>'.$film->director.'</div>
-                            </div>
-                            <form method="post" action="cesta.php">
+                        echo '<div class="responsive">';
+                            print_film($film);
+                        echo '<form method="post" action="cesta.php">
                                 <input type="hidden" name="eliminar" value="'.$film->id.'" />
                                 <input type="submit" value="eliminar">
-                            </form>
-                        </div>';    
+                            </form>';
+                        echo '</div>';
                     } 
                 ?>
                 <!-- ELEMENTOS DE LA LISTA DE LA COMPRA -->
--- 1401_Gomez_Li/php/includes/utils.php
+++ 1401_Gomez_Li/php/includes/utils.php
@@ -6,7 +6,7 @@
  * and open the template in the editor.
  */
 function print_film($films){
-     echo '<div class="responsive">
+     echo '
                     <div class="gallery">
                         <a href="product.php?id=' . $films->id . '">
                         <img src=' . $films->poster . ' alt="imagen" width="100" height="100">
@@ -16,8 +16,12 @@ function print_film($films){
                             <b>' . $films->titulo . '</b>
                         </a>
                             <br>' . $films->director . '</div>
-                    </div>
-                </div>';
+                    </div>';
      return;
 }
+function print_film_by_id($id, $xml){
+    $film = $xml->xpath("/catalogo/pelicula[id='$id']")[0];
+    print_film($film);
+}
+
 ?>
