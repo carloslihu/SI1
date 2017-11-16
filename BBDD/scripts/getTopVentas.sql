b22b3018d1e4341af88da029abe697a32fd258cc
Unknown <javiergomez_1996@hotmail.com>
2017-11-16 22:21:04 +0100

    get top ventas a medio terminar (lee descripcion)
    la query para obtener el prod_id con el maximo numero de ventas de un año esta hecha. Falta en la funcion hacer que sea para todos los años a partir de un cierto año y que la tabla que devuelva sea:
    (año, titulo de la pelicula, numero de ventas)

--- BBDD/scripts/getTopVentas.sql
+++ BBDD/scripts/getTopVentas.sql
@@ -1,24 +1,44 @@
-﻿CREATE OR REPLACE FUNCTION
+﻿drop function getTopVentas(integer);
+CREATE OR REPLACE FUNCTION
 getTopVentas(anno integer)
-RETURNS table(año double precision, pelicula varchar(255), ventas integer) AS $$
+RETURNS table(a integer,b bigint ) AS $$
 DECLARE
 	annos integer;
 BEGIN
 	
-	FOR annos IN SELECT EXTRACT(YEAR FROM orderdate) FROM orders WHERE EXTRACT(YEAR FROM orderdate) >= anno 
-			group by EXTRACT(YEAR FROM orderdate) order by EXTRACT(YEAR FROM orderdate) desc
+	FOR annos IN select extract (year from orders.orderdate) as fecha from orders
+    where extract (year from orders.orderdate) >= anno
+    group by fecha
 	LOOP
-		return query SELECT EXTRACT(YEAR FROM orderdate) as año, imdb_movies.movietitle as pelicula, inventory.sales as ventas from orders /*where orderdate >= '2008-1-1'::date*/
-		INNER JOIN orderdetail ON orders.orderid = orderdetail.orderid
-		INNER JOIN inventory ON inventory.prod_id = orderdetail.prod_id
-		INNER JOIN products ON products.prod_id = orderdetail.prod_id
-		INNER JOIN imdb_movies ON imdb_movies.movieid = products.movieid
-		where EXTRACT(YEAR FROM orderdate) = annos order by inventory.sales desc limit 1;
+		return query select prod_id, sum(quantity) from orderdetail
+    inner join orders on orderdetail.orderid = orders.orderid and
+      EXTRACT(YEAR FROM orders.orderdate) = annos
+    group by prod_id
+    order by sum desc limit 1;
 	END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 
+select * from getTopVentas(1998);
 
+
+
+sumQuantity*ventasProd
+
+select prod_id, quantity from orderdetail
+    inner join orders on orderdetail.orderid = orders.orderid and
+      EXTRACT(YEAR FROM orders.orderdate) = 2013
+      
+select prod_id, sum(quantity) as num from orderdetail
+    inner join orders on orderdetail.orderid = orders.orderid and
+      EXTRACT(YEAR FROM orders.orderdate) = 2013
+    group by prod_id
+    order by num desc limit 1
+
+ select imdb_movies.movietitle from imdb_movies, products
+  where products.prod_id = 5227 and imdb_movies.movieid = products.movieid
+
+--numero de veces que se vende el producto mas vendido de 2013
 
 /*
 
