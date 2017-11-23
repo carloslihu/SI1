36520e5426a363c8bcc248d77afa7515be61d47e
Carlos Li Hu <carloslihu96@gmail.com>
2017-11-23 17:58:18 +0100

    resubido

--- BBDD/scripts/getTopVentas.sql
+++ BBDD/scripts/getTopVentas.sql
@@ -35,28 +35,11 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-select * from getTopVentas(2011);
+/*select * from getTopVentas(2011);*/
 
 
-/*select aux.f, ventas.prod_id, aux.max from
 
-  (select f, max(sum) from 
-
-    (select EXTRACT(YEAR FROM orderdate) as f, prod_id, sum(quantity) as sum 
-      from (orderdetail
-      natural join orders) AS o
-      group by f,prod_id) AS v
-    
-  group by f) as aux
-
-  inner join (
-  select EXTRACT(YEAR FROM orders.orderdate) as f, prod_id, sum(quantity) 
-    from orderdetail
-    natural join orders
-    group by f,prod_id) as ventas
-  on ventas.f = aux.f and ventas.sum = aux.max
-*/
-SELECT f,movieid,total3
+/*SELECT f,movieid,total3
 FROM (
   SELECT f,MAX(total2) as total3
     FROM (
@@ -79,39 +62,6 @@ NATURAL JOIN --tabla donde tengo total3 esta con la peli
         GROUP BY f, prod_id) AS q--fechas y productos y cantidad de veces que se vendio en esa fecha
     NATURAL JOIN products
     GROUP BY (f,movieid)) AS q4
-WHERE total2=total3;    
-  
-
-(select EXTRACT(YEAR FROM orders.orderdate) as f from orders group by f)
-
-select prod_id, quantity from orderdetail
-    inner join orders on orderdetail.orderid = orders.orderid and
-      EXTRACT(YEAR FROM orders.orderdate) = 2013
-      
-select prod_id, sum(quantity) as num from orderdetail
-    inner join orders on orderdetail.orderid = orders.orderid and
-      EXTRACT(YEAR FROM orders.orderdate) = 2013
-    group by prod_id
-    order by num desc limit 1
-
- select imdb_movies.movietitle from imdb_movies, products
-  where products.prod_id = 5227 and imdb_movies.movieid = products.movieid
+WHERE total2=total3;    */
 
 --numero de veces que se vende el producto mas vendido de 2013
-
-/*
-
-
-SELECT EXTRACT(YEAR FROM orderdate) FROM orders WHERE orderdate >= '2008-1-1'::date group by EXTRACT(YEAR FROM orderdate) order by EXTRACT(YEAR FROM orderdate) desc
-
-
-
-SELECT distinct EXTRACT(YEAR FROM orderdate), imdb_movies.movietitle, max(inventory.sales) from orders
-INNER JOIN orderdetail ON orders.orderid = orderdetail.orderid
-INNER JOIN inventory ON inventory.prod_id = orderdetail.prod_id
-INNER JOIN products ON products.prod_id = orderdetail.prod_id
-INNER JOIN imdb_movies ON imdb_movies.movieid = products.movieid
-where EXTRACT(YEAR FROM orderdate) >= 2008 group by EXTRACT(YEAR FROM orderdate), imdb_movies.movietitle order by EXTRACT(YEAR FROM orderdate) desc
-
-SELECT prod_id from orderdetail where 
-*/
