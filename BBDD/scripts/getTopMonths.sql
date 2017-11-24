df5d713b47a024a0a40d2c7205f8365822195b8d
Unknown <javiergomez_1996@hotmail.com>
2017-11-24 16:08:27 +0100

    me faltaba commitear el getTopMonths, gracias por avisar ;)

--- BBDD/scripts/getTopMonths.sql
+++ BBDD/scripts/getTopMonths.sql
@@ -1,49 +1,40 @@
-﻿DROP function gettopmonths(integer,numeric);
+﻿--DROP function gettopmonths(integer,numeric);
 CREATE OR REPLACE FUNCTION
 getTopMonths(nProducts integer, importe numeric)
-RETURNS table(y double precision, m double precision) AS $$
+RETURNS table(fecha text, money numeric, sales bigint) AS $$
 DECLARE
 
 BEGIN
-  RETURN QUERY SELECT a.yy, a.mm from 
-    (SELECT
-      EXTRACT (YEAR from orders.orderdate) as yy,
-      EXTRACT (MONTH from orders.orderdate) as mm,
-      sum(totalamount) as totamount
-      from orders
-    group by yy, mm) as a
-    INNER JOIN 
-      (SELECT
-        EXTRACT (YEAR from orders.orderdate) as yy,
-        EXTRACT (MONTH from orders.orderdate) as mm,
-        sum(inventory.sales) as totsales
-        from orders
-      INNER JOIN orderdetail ON orders.orderid = orderdetail.orderid
-      INNER JOIN inventory ON orderdetail.prod_id = inventory.prod_id
-      group by mm, yy
-      order by yy, mm) as b
-    ON a.yy = b.yy and a.mm = a.mm
-  where totamount >= importe or totsales >= nProducts
-  group by a.yy, a.mm
-  order by a.yy, a.mm;
+  RETURN QUERY SELECT * from
+    (SELECT to_char(orderdate, 'YYYY-MM') as fecha, sum(totalamount) as total from orders
+      group by fecha) as amount
+  natural join
+    (SELECT to_char(orderdate, 'YYYY-MM') as fecha, sum(sales) as ventas 
+      from inventory
+      NATURAL JOIN orderdetail
+      NATURAL JOIN orders
+      group by fecha) as sale
+  where total >= importe or ventas >= nProducts;
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT * FROM getTopMonths(19000, 320000)
+SELECT * FROM getTopMonths(19000, 320000) order by fecha
 --query para obtener la ganancia de todos los meses tales que esta sea mayor que <<tal>>
-SELECT max(tot) from 
+SELECT fecha, max(tot) as totalamount from 
   (SELECT to_char(orderdate, 'YYYY-MM') as fecha, sum(totalamount) as tot from orders
   group by to_char(orderdate, 'YYYY-MM')) as b
-where tot > 200000
+group by fecha
 
+SELECT setOrderAmount();
+  
 SELECT EXTRACT (YEAR from orders.orderdate) as yy,
         EXTRACT (MONTH from orders.orderdate) as mm, sum(totalamount) from orders group by yy, mm
 
 --query para obtener el numero de ventas de todos los meses
-SELECT max(tot) from (
-SELECT to_char(orderdate, 'YYYY-MM') as fecha, sum(inventory.sales) as tot from orders
-INNER JOIN orderdetail ON orders.orderid = orderdetail.orderid
-INNER JOIN inventory ON orderdetail.prod_id = inventory.prod_id
+SELECT fecha, max(sales) as sales from (
+SELECT to_char(orderdate, 'YYYY-MM') as fecha, sum(inventory.sales) as sales from orders
+NATURAL JOIN orderdetail
+NATURAL JOIN inventory
 group by to_char(orderdate, 'YYYY-MM')
 order by to_char(orderdate, 'YYYY-MM')) as b
-where tot > 340000;
+group by fecha;
