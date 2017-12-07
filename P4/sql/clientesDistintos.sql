3ed93e8ba4fed21ae192ad813a6da0f2051497ea
Carlos Li Hu <carloslihu96@gmail.com>
2017-12-07 13:51:03 +0100

    semana 1 hecha

--- P4/sql/clientesDistintos.sql
+++ P4/sql/clientesDistintos.sql
@@ -4,9 +4,14 @@ WHERE totalamount > 100 AND
 EXTRACT(MONTH FROM orderdate)=4 AND 
 EXTRACT(YEAR FROM orderdate)=2014;
 
+
+DROP INDEX IF EXISTS idx_customerid_customers;
 DROP INDEX IF EXISTS idx_orderdate_orders;
 DROP INDEX IF EXISTS idx_totalamount_orders;
-DROP INDEX IF EXISTS idx_customerid_customers;
-CREATE INDEX idx_orderdate_orders ON orders(orderdate);
+
+--CREATE INDEX idx_customerid_customers ON customers(customerid);
+--CREATE INDEX idx_orderdate_orders ON orders(orderdate);
 CREATE INDEX idx_totalamount_orders ON orders(totalamount);
-CREATE INDEX idx_customerid_customers ON customers(customerid);
+ANALYZE orders;
+ANALYZE customers;
+
--- P4/sql/countStatus.sql
+++ P4/sql/countStatus.sql
@@ -1,4 +1,6 @@
- --APARTADO D
+﻿ --APARTADO D
+
+ --CONSULTAS
  select count(*)
 from orders
 where status is null;
@@ -6,22 +8,6 @@ where status is null;
 select count(*)
 from orders
 where status ='Shipped';
-
---Mio
-DROP INDEX IF EXISTS idx_status_orders;
-CREATE INDEX idx_status_orders ON orders(status);
-
-ANALYZE orders
-
-
-
-
-
---Estudiar, explicar y comparar la planificación de las dos consultas
---• sobre una base de datos limpia (recién creada y cargada de datos),
---• tras crear un índice
---• y tras generar las estadísticas con la sentencia ANALYZE.
---Comparar también con la planificación de las siguientes consultas, una vez generadas las estadísticas:
 
 select count(*)
 from orders
@@ -31,8 +17,8 @@ select count(*)
 from orders
 where status ='Processed';
 
---Ayuda:
---• ¿Qué hace el generador de estadísticas?
---• Usar la sentencia ANALYZE, no VACUUM ANALYZE
---• EXPLAIN ANALYZE no calcula estadísticas. El cálculo de estadísticas se realiza con la
---sentencia ANALYZE
+--MI CODIGO
+DROP INDEX IF EXISTS idx_status_orders;
+CREATE INDEX idx_status_orders ON orders(status);
+
+ANALYZE VERBOSE orders;
