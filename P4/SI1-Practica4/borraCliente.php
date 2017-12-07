49f806b9b147cd40e5cdaab57b26d7f34f6167f1
Carlos Li Hu <carloslihu96@gmail.com>
2017-12-07 16:57:47 +0100

    consulta borra cosas

---
+++ P4/SI1-Practica4/borraCliente.php
@@ -0,0 +1,42 @@
+<?php
+define("PGUSER", "alumnodb");
+define("PGPASSWORD", "alumnodb");
+define("DSN", "pgsql:host=localhost;dbname=si1;options='--client_encoding=UTF8'");
+?>
+<?php
+if (!isset($_REQUEST['submit'])) {
+    ?>
+    <form action="" method="get">
+        customerid<input type="number" name="customerid">
+        <br>
+
+        <input type="submit" name="submit" value="Enviar">
+    </form>
+    <?php
+} else {
+    try {
+        $db = new PDO(DSN, PGUSER, PGPASSWORD);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->beginTransaction();
+
+        $sql = "DELETE FROM orderdetail WHERE orderid IN "
+                . "(SELECT orderid FROM orders WHERE customerid=" . $_REQUEST['customerid'] . ");";
+        $result = $db->exec($sql);
+
+        $sql = "DELETE FROM orders WHERE customerid=" . $_REQUEST['customerid'];
+        $result = $db->exec($sql);
+
+        $sql = "DELETE FROM customers WHERE customerid=" . $_REQUEST['customerid'];
+        $result = $db->exec($sql);
+
+        $db->commit();
+        echo '<p><b>Borrado correcto</b></p>';
+        echo '<p><a href="borraCliente.php">Nueva consulta</a></p>';
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        $db->rollBack();
+    }
+
+    $db = null;
+}
+?>
